c5e851c196c319c0fac21fc0f6d5f8131798ef16
Vlad <vlasisla1988@gmail.com>
2024-03-28 22:40:05 +0200

    Updates

---
+++ social-API/social-sharing.js
@@ -0,0 +1,8 @@
+
+// 'WhatsApp',
+// set this in the link  '//wa.me/?text=', + your site url
+
+// Facebook
+// set this in the link 'https://www.facebook.com/sharer.php?u=', + your site url
+
+// for react - from 'react-share' lib
